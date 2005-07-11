1316e3ab69fe3e08de6d6f08a04ce0f4bd94dc04
Johan Dahlin <jdahlin@async.com.br>
2005-07-11 20:36:06 +0000

    Extend example to include non-ASCII characters

--- examples/completion.py
+++ examples/completion.py
@@ -1,10 +1,21 @@
+# encoding: iso-8859-1
 import gtk
 
 from kiwi.ui.widgets.entry import Entry
 
+def on_entry_activate(entry):
+    print 'You selected:', entry.get_text().encode('latin1')
+    gtk.main_quit()
+
 entry = Entry()
-entry.set_completion_strings(['apa', 'apapa', 'apbla',
-                              'apppa', 'aaspa'])
+entry.connect('activate', on_entry_activate)
+entry.set_completion_strings(['Belo Horizonte',
+                              u'São Carlos',
+                              u'São Paulo',
+                              u'Båstad',
+                              u'Örnsköldsvik',
+                              'sanca',
+                              'sampa'])
 
 win = gtk.Window()
 win.connect('delete-event', gtk.main_quit)
