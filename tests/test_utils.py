868761a034ba35e4da3c9cc16a59fd81104bd342
Johan Dahlin <jdahlin@async.com.br>
2005-07-18 14:27:39 +0000

    Add test

---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+import unittest
+
+from kiwi.utils import slicerange
+
+class SliceTest(unittest.TestCase):
+    def genlist(self, limit, start, stop=None, step=None):
+        if stop == None:
+            stop = start
+            start = None
+            
+        return list(slicerange(slice(start, stop, step), limit))
+    
+    def testStop(self):
+        self.assertEqual(self.genlist(10, 10), range(10))
+        self.assertEqual(self.genlist(10, -5), range(5))
+        self.assertEqual(self.genlist(10, -15), [])
+        self.assertEqual(self.genlist(5, 10), range(5))
+        self.assertEqual(self.genlist(0, 10), [])
+
+    def testStartStop(self):
+        self.assertEqual(self.genlist(10, 0, 10), range(10))
+        self.assertEqual(self.genlist(10, 1, 9), range(10)[1:9])
+        self.assertEqual(self.genlist(10, -1, -1), range(10)[-1:-1])
+        self.assertEqual(self.genlist(10, 0, -15), range(10)[0:-15])
+        self.assertEqual(self.genlist(10, 15, 0), range(10)[-15:0])
+        
+    def testStartStopStep(self):
+        self.assertEqual(self.genlist(10, 0, 10, 2), range(10)[0:10:2])
+
+if __name__ == '__main__':
+    unittest.main()
