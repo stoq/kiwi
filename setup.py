a5138ebb18df7b8f907d850c2c2aa48a340bf3a8
Johan Dahlin <jdahlin@async.com.br>
2005-07-18 14:27:07 +0000

    Rename to kiwi (from kiwi2), update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@
 # Code by Async Open Source <http://www.async.com.br>
 # setup.py writen by Christian Reis <kiko@async.com.br>
 
+from fnmatch import fnmatch
+import os
 from distutils.core import setup
+
+def listfiles(*dirs):
+    dir, pattern = os.path.split(os.path.join(*dirs))
+    return [os.path.join(dir, filename)
+            for filename in os.listdir(os.path.abspath(dir))
+                if filename[0] != '.' and fnmatch(filename, pattern)]
 
 execfile("kiwi/__version__.py")
 
@@ -22,10 +30,16 @@ setup(
     author_email = "kiko@async.com.br",
     url = "http://www.async.com.br/projects/",
     license = "GNU LGPL 2.1 (see COPYING)",
-
+    data_files = [
+        ('share/gazpacho/catalogs',
+         listfiles('gazpacho-plugin', 'kiwi.xml')),
+        ('share/gazpacho/resources/kiwi',
+         listfiles('gazpacho-plugin', 'pixmaps', '*.png')),
+        ('lib/gazpacho/widgets',
+         listfiles('gazpacho-plugin', 'kiwi.py')),
+        ],
     packages = ['kiwi',
                 'kiwi.ui',
                 'kiwi.ui.widgets'],
     package_dir = {'kiwi':'kiwi'},
     )
-
