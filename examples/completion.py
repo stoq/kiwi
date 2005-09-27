97d5c5b8fd7d5390199529a5e65a97b9f558c134
Johan Dahlin <jdahlin@async.com.br>
2005-09-27 14:59:10 +0000

    Add an entry completion example

--- examples/completion.py
+++ examples/completion.py
@@ -4,22 +4,33 @@ import gtk
 from kiwi.ui.widgets.entry import Entry
 
 def on_entry_activate(entry):
-    print 'You selected:', entry.get_text().encode('latin1')
+    print 'You selected:', entry.read()
     gtk.main_quit()
-
-entry = Entry()
-entry.connect('activate', on_entry_activate)
-entry.set_completion_strings(['Belo Horizonte',
-                              u'São Carlos',
-                              u'São Paulo',
-                              u'Båstad',
-                              u'Örnsköldsvik',
-                              'sanca',
-                              'sampa'])
 
 win = gtk.Window()
 win.connect('delete-event', gtk.main_quit)
-win.add(entry)
+
+vbox = gtk.VBox()
+win.add(vbox)
+
+# Normal entry
+entry = Entry()
+entry.connect('activate', on_entry_activate)
+entry.set_completion_strings(['Belo Horizonte', u'São Carlos',
+                              u'São Paulo',  u'Båstad',
+                              u'Örnsköldsvik', 'sanca', 'sampa'])
+vbox.pack_start(entry)
+
+
+entry = Entry()
+entry.connect('activate', on_entry_activate)
+entry.set_completion_strings(['Brazil', 'Sweden', 'China'],
+                             [186, 9, 1306])
+entry.set_completion_strings()
+vbox.pack_start(entry)
+
 win.show_all()
+
+
 
 gtk.main()
