46a520b7a62fbebd0ecdc0f75c34de67ed8598b5
Johan Dahlin <jdahlin@async.com.br>
2005-10-10 13:49:57 +0000

    a couple of bugfixes for Application loading. Copy over namedAny from twisted, because it's useful

--- kiwi/environ.py
+++ kiwi/environ.py
@@ -30,6 +30,39 @@ import sys
 __all__ = ['Application', 'Library', 'environ', 'require_gazpacho',
            'is_gazpacho_required']
 
+# copied from twisted/python/reflect.py
+def namedAny(name):
+    """Get a fully named package, module, module-global object, or attribute.
+    """
+    names = name.split('.')
+    topLevelPackage = None
+    moduleNames = names[:]
+    while not topLevelPackage:
+        try:
+            trialname = '.'.join(moduleNames)
+            topLevelPackage = __import__(trialname)
+        except ImportError:
+            # if the ImportError happened in the module being imported,
+            # this is a failure that should be handed to our caller.
+            # count stack frames to tell the difference.
+            import traceback
+            exc_info = sys.exc_info()
+            if len(traceback.extract_tb(exc_info[2])) > 1:
+                try:
+                    # Clean up garbage left in sys.modules.
+                    del sys.modules[trialname]
+                except KeyError:
+                    # Python 2.4 has fixed this.  Yay!
+                    pass
+                raise exc_info[0], exc_info[1], exc_info[2]
+            moduleNames.pop()
+    
+    obj = topLevelPackage
+    for n in names[1:]:
+        obj = getattr(obj, n)
+        
+    return obj
+
 class Environment:
     """Environment control
 
@@ -44,7 +77,10 @@ class Environment:
         
         self._add_resource_variable("glade", "KIWI_GLADE_PATH")
         self._add_resource_variable("image", "KIWI_IMAGE_PATH")
-        
+
+    def get_root(self):
+        return self._root
+    
     def _add_resource_variable(self, resource, variable):
         """Add resources from an environment variable"""
         env = os.environ.get(variable, '')
@@ -90,12 +126,13 @@ class Library(Environment):
     It provides a way to manage local resources, which should only be seen
     in the current context.
     """
-    def __init__(self, name, root='..'):
+    def __init__(self, name, root='..', dirname=None):
         self.name = name
-        # Figure out the absolute path to the caller
-        caller = sys._getframe(1).f_locals['__file__']
-        caller_abs = os.path.split(caller)[0]
-        root = os.path.abspath(os.path.join(caller_abs, root))
+        if dirname == None:
+            # Figure out the absolute path to the caller
+            caller = sys._getframe(1).f_locals['__file__']
+            dirname = os.path.split(caller)[0]
+        root = os.path.abspath(os.path.join(dirname, root))
 
         Environment.__init__(self, root=root)
         
@@ -145,24 +182,21 @@ class Application(Library):
     - path, a reference to the callable object to run, defaults to 'main'
     """
     def __init__(self, name, root='..', path='main'):
-        Library.__init__(self, name, root)
+        dirname = os.path.abspath(os.path.dirname(sys.argv[0]))
+        Library.__init__(self, name, root, dirname)
         self._path = path
 
     def _get_main(self):
-        path = self._path
-        main_module = os.path.join(self.root, path)
-
         try:
-            module = __import__(main_module, globals(), locals(), path)
-        except Exception, e:
-            raise SystemExit("ERROR: Failed to import required module %s\n\n"
-                             "Exception raised during import:\n %s: %s\n" %
-                             (main_module, e.__class__.__name__, e))
-
-        main = getattr(module, path, None)
+            module = namedAny(self._path)
+        except (ValueError, AttributeError, ImportError), e:
+            raise SystemExit("ERROR: Could not find item '%s', %s" %
+                             (self._path, e))
+            
+        main = getattr(module, 'main', None)
         if not main or not callable(main):
             raise SystemExit("ERROR: Could not find item '%s' in module %s" %
-                             path, main_module)
+                             'main', path)
         return main
 
     def enable_translation(self, domain=None, locale='locale',
