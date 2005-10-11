da59cb10b4d060df5a3af9dccf4c869a4bf48915
Johan Dahlin <jdahlin@async.com.br>
2005-10-11 15:42:22 +0000

    Oops, really move over deprecated

--- kiwi/utils.py
+++ kiwi/utils.py
@@ -27,7 +27,6 @@ import sys
 
 import gobject
 
-from kiwi import _warn
 from kiwi.python import ClassInittableObject
 
 def list_properties(gtype, parent=True):
@@ -43,16 +42,6 @@ def list_properties(gtype, parent=True):
     parent_pspecs = gobject.list_properties(parent)
     return [pspec for pspec in pspecs
                       if pspec not in parent_pspecs]
-
-class deprecated(object):
-    def __init__(self, new):
-        self.new = new
-
-    def __call__(self, func):
-        def wrapper(*args, **kwargs):
-            _warn("%s is deprecated, use %s instead" % (func.__name__, self.new))
-            return func(*args, **kwargs)
-        return wrapper
 
 class PropertyObject(ClassInittableObject):
     """
