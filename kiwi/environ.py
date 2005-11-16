90e2c06b28ca7228e019b926b526264f18042014
Johan Dahlin <jdahlin@async.com.br>
2005-11-16 19:47:44 +0000

    move namedAny from environ to python

--- kiwi/environ.py
+++ kiwi/environ.py
@@ -27,41 +27,10 @@ import gettext
 import os
 import sys
 
+from kiwi.python import namedAny
+
 __all__ = ['Application', 'Library', 'app', 'environ', 'require_gazpacho',
            'is_gazpacho_required']
-
-# copied from twisted/python/reflect.py
-def namedAny(name):
-    """Get a fully named package, module, module-global object, or attribute.
-    """
-    names = name.split('.')
-    topLevelPackage = None
-    moduleNames = names[:]
-    while not topLevelPackage:
-        try:
-            trialname = '.'.join(moduleNames)
-            topLevelPackage = __import__(trialname)
-        except ImportError:
-            # if the ImportError happened in the module being imported,
-            # this is a failure that should be handed to our caller.
-            # count stack frames to tell the difference.
-            import traceback
-            exc_info = sys.exc_info()
-            if len(traceback.extract_tb(exc_info[2])) > 1:
-                try:
-                    # Clean up garbage left in sys.modules.
-                    del sys.modules[trialname]
-                except KeyError:
-                    # Python 2.4 has fixed this.  Yay!
-                    pass
-                raise exc_info[0], exc_info[1], exc_info[2]
-            moduleNames.pop()
-    
-    obj = topLevelPackage
-    for n in names[1:]:
-        obj = getattr(obj, n)
-        
-    return obj
 
 class Environment:
     """Environment control
--- kiwi/python.py
+++ kiwi/python.py
@@ -21,6 +21,9 @@
 # Author(s): Johan Dahlin <jdahlin@async.com.br>
 #         
 
+import os
+import sys
+
 import gobject
 
 # When we can depend on 2.8 clean this up, so ClassInittable does not
@@ -60,6 +63,39 @@ class ClassInittableObject(object):
         """
     __class_init__ = classmethod(__class_init__)
 
+
+# copied from twisted/python/reflect.py
+def namedAny(name):
+    """Get a fully named package, module, module-global object, or attribute.
+    """
+    names = name.split('.')
+    topLevelPackage = None
+    moduleNames = names[:]
+    while not topLevelPackage:
+        try:
+            trialname = '.'.join(moduleNames)
+            topLevelPackage = __import__(trialname)
+        except ImportError:
+            # if the ImportError happened in the module being imported,
+            # this is a failure that should be handed to our caller.
+            # count stack frames to tell the difference.
+            import traceback
+            exc_info = sys.exc_info()
+            if len(traceback.extract_tb(exc_info[2])) > 1:
+                try:
+                    # Clean up garbage left in sys.modules.
+                    del sys.modules[trialname]
+                except KeyError:
+                    # Python 2.4 has fixed this.  Yay!
+                    pass
+                raise exc_info[0], exc_info[1], exc_info[2]
+            moduleNames.pop()
+    
+    obj = topLevelPackage
+    for n in names[1:]:
+        obj = getattr(obj, n)
+        
+    return obj
 
 def clamp(x, low, high):
     """
