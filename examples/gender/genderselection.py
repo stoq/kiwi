3914ff0a778601dca6883e7b4cfbda90cb6d5c2a
Johan Dahlin <jdahlin@async.com.br>
2005-12-15 12:29:27 +0000

    Rename SexSelection to gender and make it work again

---
+++ examples/gender/genderselection.py
@@ -0,0 +1,21 @@
+import gtk
+
+from kiwi.ui.views import BaseView, quit_if_last
+
+class Person:
+    gender = 'Male'
+
+class GenderSelection(BaseView):
+    gladefile = 'gender'
+    widgets = ['male', 'female', 'other']
+
+    def __init__(self):
+        BaseView.__init__(self, delete_handler=quit_if_last)
+        self.model = Person()
+        self.add_proxy(self.model, self.widgets)
+          
+view = GenderSelection()
+view.show_all()
+gtk.main()
+print view.model.gender
+
