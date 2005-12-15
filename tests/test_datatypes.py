c2c4fd107de79dbdbba5fb70aa1604c4a27f94d2
Johan Dahlin <jdahlin@async.com.br>
2005-12-15 13:55:07 +0000

    currency & converter updates

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -2,15 +2,16 @@ from datetime import date
 import unittest
 import locale
 
-from kiwi import datatypes
+from kiwi.datatypes import currency, converter, ValidationError
 
 import utils
+
+date_converter = converter.get_converter(date)
+bool_converter = converter.get_converter(bool)
 
 class DataTypesTest(unittest.TestCase):
 
     def teststr2bool(self):
-        bool_converter = datatypes.BoolConverter()
-
         self.assertEqual(bool_converter.from_string('TRUE'), True)
         self.assertEqual(bool_converter.from_string('true'), True)
         self.assertEqual(bool_converter.from_string('TrUe'), True)
@@ -26,7 +27,6 @@ class DataTypesTest(unittest.TestCase):
     def teststr2date(self):
         # set the date format to the spanish one
         locale.setlocale(locale.LC_TIME, 'es_ES')
-        date_converter = datatypes.DateConverter()
 
         birthdate = date(1979, 2, 12)
         # in the spanish locale the format of a date is %d/%m/%y
@@ -35,29 +35,26 @@ class DataTypesTest(unittest.TestCase):
 
         # let's try with the portuguese locale
         locale.setlocale(locale.LC_TIME, 'pt_BR')
-        date_converter.update_format()
 
         # portuguese format is "%d-%m-%Y"
         self.assertEqual(date_converter.from_string("12-2-1979"), birthdate)
         self.assertEqual(date_converter.from_string("12-02-1979"), birthdate)
 
         # test some invalid dates
-        self.assertRaises(datatypes.ValidationError,
+        self.assertRaises(ValidationError,
                           date_converter.from_string, "40-10-2005")
         # february only have 28 days
-        self.assertRaises(datatypes.ValidationError,
+        self.assertRaises(ValidationError,
                           date_converter.from_string, "30-02-2005")
         
     def testdate2str(self):
         locale.setlocale(locale.LC_TIME, 'es_ES')
-        date_converter = datatypes.DateConverter()
 
         birthdate = date(1979, 2, 12)
 
         self.assertEqual(date_converter.as_string(birthdate), "12/02/79")
 
         locale.setlocale(locale.LC_TIME, 'pt_BR')
-        date_converter.update_format()
 
         self.assertEqual(date_converter.as_string(birthdate), "12-02-1979")
 
@@ -65,21 +62,21 @@ class DataTypesTest(unittest.TestCase):
         self.assertEqual(locale.setlocale(locale.LC_MONETARY, 'pt_BR'),
                          'pt_BR',
                          'pt_BR locale is required to run this test')
-        self.assertEqual(datatypes.format_price(100), 'R$100')
-        self.assertEqual(datatypes.format_price(123.45), 'R$123,45')
-        self.assertEqual(datatypes.format_price(12345), 'R$12.345')
-        self.assertEqual(datatypes.format_price(-100), 'R$-100')
+        self.assertEqual(currency(100).format(), 'R$100')
+        self.assertEqual(currency(123.45).format(), 'R$123,45')
+        self.assertEqual(currency(12345).format(), 'R$12.345')
+        self.assertEqual(currency(-100).format(), 'R$-100')
 
     def testFormatPriceEnUS(self):
         self.assertEqual(locale.setlocale(locale.LC_MONETARY, 'en_US'),
                          'en_US',
                          'en_US locale is required to run this test')
-        self.assertEqual(datatypes.format_price(100), '$100')
-        self.assertEqual(datatypes.format_price(123.45), '$123.45')
-        self.assertEqual(datatypes.format_price(12345), '$12,345')
-        self.assertEqual(datatypes.format_price(-100), '$-100')
-        self.assertEqual(datatypes.format_price(1, True), '$1')
-        self.assertEqual(datatypes.format_price(1, False), '1')
+        self.assertEqual(currency(100).format(), '$100')
+        self.assertEqual(currency(123.45).format(), '$123.45')
+        self.assertEqual(currency(12345).format(), '$12,345')
+        self.assertEqual(currency(-100).format(), '$-100')
+        self.assertEqual(currency(1).format(True), '$1')
+        self.assertEqual(currency(1).format(False), '1')
 
 if __name__ == "__main__":
     unittest.main()
