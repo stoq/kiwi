59bbd3d079f1586328eba66e8c51d57b3d6e8c0e
Johan Dahlin <jdahlin@async.com.br>
2005-12-15 12:52:29 +0000

    Add a couple of list examples and rename the old one to kitchensink

---
+++ examples/list/editable.py
@@ -0,0 +1,30 @@
+import gtk
+
+from kiwi.ui.widgets.list import Column, List
+
+class Fruit:
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
+        
+fruits = List([Column('name', data_type=str, editable=True,
+                      expand=True),
+               Column('cost', data_type=int, editable=True)])
+
+for name, cost in [('Apple', 4),
+                   ('Pineapple', 2),
+                   ('Kiwi', 8),
+                   ('Banana', 3),
+                   ('Melon', 5)]:
+    fruits.append(Fruit(name, cost))
+
+window = gtk.Window()
+window.connect('delete-event', gtk.main_quit)
+window.set_title('Editable Fruit List')
+window.set_size_request(230, 150)
+
+window.add(fruits)
+window.show_all()
+
+gtk.main()
+
---
+++ examples/list/simple.py
@@ -0,0 +1,29 @@
+import gtk
+
+from kiwi.ui.widgets.list import Column, List
+
+class Fruit:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+fruits = List([Column('name', data_type=str),
+               Column('price', data_type=int)])
+
+for name, price in [('Apple', 4),
+                    ('Pineapple', 2),
+                    ('Kiwi', 8),
+                    ('Banana', 3),
+                    ('Melon', 5)]:
+    fruits.append(Fruit(name, price))
+
+window = gtk.Window()
+window.connect('delete-event', gtk.main_quit)
+window.set_title('Fruits')
+window.set_size_request(150, 150)
+
+window.add(fruits)
+window.show_all()
+
+gtk.main()
+
