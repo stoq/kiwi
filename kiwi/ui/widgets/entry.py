f56cb8d5ba9d71772c168f6f4dd2b1e36249a2aa
Johan Dahlin <jdahlin@async.com.br>
2006-01-27 18:58:07 +0000

    whitespace

--- kiwi/ui/widgets/entry.py
+++ kiwi/ui/widgets/entry.py
@@ -7,17 +7,17 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 # USA
-# 
+#
 # Author(s): Christian Reis <kiko@async.com.br>
 #            Lorenzo Gil Sanchez <lgs@sicem.biz>
 #            Gustavo Rahal <gustavo@async.com.br>
@@ -48,10 +48,10 @@ _ = gettext.gettext
 class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
     """The Kiwi Entry widget has many special features that extend the basic
     gtk entry.
-    
+
     First of all, as every Kiwi Widget, it implements the Proxy protocol.
     As the users types the entry can interact with the application model
-    automatically. 
+    automatically.
     Kiwi Entry also implements interesting UI additions. If the input data
     does not match the data type of the entry the background nicely fades
     to a light red color. As the background changes an information icon
@@ -63,7 +63,7 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
 
     gproperty("completion", bool, False)
     gproperty('exact-completion', bool, default=False)
-        
+
     def __init__(self, data_type=None):
         gtk.Entry.__init__(self)
         WidgetMixinSupportValidation.__init__(self)
@@ -96,23 +96,23 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
         completion.set_match_func(match_func)
 
         return value
-    
+
     def prop_set_completion(self, value):
         if not self.get_completion():
             self._enable_completion()
         return value
-    
+
     # Public API
     def set_exact_completion(self, value):
         """
         Enable exact entry completion.
         Exact means it needs to start with the value typed
         and the case needs to be correct.
-        
+
         @param value: enable exact completion
         @type value:  boolean
         """
-        
+
         self.exact_completion = value
 
     # XXX: Decide if this API or the Combobox prefill API should be used
@@ -121,7 +121,7 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
         Set strings used for entry completion.
         If values are provided, each string will have an additional
         data type.
-        
+
         @param strings:
         @type  strings: list of strings
         @param values:
@@ -131,11 +131,11 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
         completion = self._create_completion()
         model = completion.get_model()
         model.clear()
-        
+
         if values:
             if len(strings) != len(values):
                 raise ValueError("values must have the same length as strings")
-                
+
             for i, text in enumerate(strings):
                 model.append([text, values[i]])
             self._entry_mode = ENTRY_MODE_DATA
@@ -150,7 +150,7 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
 
     def set_text(self, text):
         self._update_current_object(text)
-                
+
         gtk.Entry.set_text(self, text)
         self.emit('content-changed')
 
@@ -176,11 +176,11 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
     def _get_text_from_object(self, obj):
         if self._entry_mode != ENTRY_MODE_DATA:
             return
-        
+
         for row in self.get_completion().get_model():
             if row[COL_OBJECT] == obj:
                 return row[COL_TEXT]
-        
+
     def _create_completion(self):
         # Check so we have completion enabled, not this does not
         # depend on the property, the user can manually override it,
@@ -188,9 +188,9 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
         completion = self.get_completion()
         if completion:
             return completion
-        
+
         return self._enable_completion()
-        
+
     def _enable_completion(self):
         completion = gtk.EntryCompletion()
         self.set_completion(completion)
@@ -201,7 +201,7 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
                            self._on_completion__match_selected)
         self._current_object = None
         return completion
-    
+
     def _completion_exact_match_func(self, completion, _, iter):
         model = completion.get_model()
         if not len(model):
@@ -223,11 +223,11 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
             return
 
         # this updates current_object and triggers content-changed
-        self.set_text(model[iter][COL_TEXT]) 
+        self.set_text(model[iter][COL_TEXT])
         self.set_position(-1)
         # FIXME: Enable this at some point
         #self.activate()
-        
+
     def read(self):
         mode = self._entry_mode
         if mode == ENTRY_MODE_TEXT:
@@ -236,7 +236,7 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
             return self._current_object
         else:
             raise AssertionError
-    
+
     def update(self, data):
         if data is None:
             text = ""
@@ -262,28 +262,28 @@ class Entry(PropertyObject, gtk.Entry, WidgetMixinSupportValidation):
     def do_size_allocate(self, allocation):
         #gtk.Entry.do_size_allocate(self, allocation)
         self.chain(allocation)
-	
+
 	if self.flags() & gtk.REALIZED:
             self._icon.resize_windows()
 
     def do_realize(self):
         gtk.Entry.do_realize(self)
         self._icon.construct()
-        
+
     def do_unrealize(self):
         self._icon.deconstruct()
         gtk.Entry.do_unrealize(self)
-        
+
     # IconEntry
-    
+
     def set_pixbuf(self, pixbuf):
         self._icon.set_pixbuf(pixbuf)
-        
+
     def update_background(self, color):
         self._icon.update_background(color)
-        
+
     def get_icon_window(self):
         return self._icon.get_icon_window()
 
 type_register(Entry)
-    
+
