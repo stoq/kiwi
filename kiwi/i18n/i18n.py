3171a6b08d2268c37f87a084ca81d8759b4d93fd
Johan Dahlin <jdahlin@async.com.br>
2006-01-27 16:48:30 +0000

    whitespace

--- kiwi/i18n/i18n.py
+++ kiwi/i18n/i18n.py
@@ -7,17 +7,17 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 # USA
-# 
+#
 
 """Internationalization utilities. Requires intltool and gettext"""
 
@@ -46,7 +46,7 @@ def check_directory(root):
     if os.path.exists(locale_dir):
         if not os.path.isdir(po_dir):
             raise SystemExit("locale must be a directory")
-        
+
 def check_pot_file(root, package):
     pot_file = os.path.join(root, 'po', '%s.pot' % package)
     if not os.path.exists(pot_file):
@@ -69,7 +69,7 @@ def get_translatable_files(root):
 def list_languages(root):
     return [os.path.basename(po_file[:-3])
                 for po_file in listfiles(root, 'po', '*.po')]
-        
+
 def update_po(root, package):
     files = get_translatable_files(root)
     potfiles_in = os.path.join(root, 'po', 'POTFILES.in')
@@ -83,10 +83,10 @@ def update_po(root, package):
 
     if os.system('intltool-update 2> /dev/null') != 0:
         raise SystemExit('ERROR: intltool-update could not be found')
-    
+
     # POT file first
     os.system('intltool-update --pot --gettext-package=%s' % package)
-    
+
     for lang in list_languages(root):
         new = lang + '.new.po'
         os.system('intltool-update --dist --gettext-package=%s '
@@ -96,23 +96,23 @@ def update_po(root, package):
     os.chdir(old)
 
     os.unlink(potfiles_in)
-        
+
 def compile_po_files(root, package):
     if os.system('msgfmt 2> /dev/null') != 256:
         print 'msgfmt could not be found, disabling translations'
         return
-    
+
     mo_file = package + '.mo'
     for po_file in listfiles(root, 'po', '*.po'):
         lang = os.path.basename(po_file[:-3])
         mo = os.path.join(root, 'locale', lang, 'LC_MESSAGES', mo_file)
-        
+
         if not os.path.exists(mo) or newer(po_file, mo):
             directory = os.path.dirname(mo)
             if not os.path.exists(directory):
                 os.makedirs(directory)
             os.system('msgfmt %s -o %s' % (po_file, mo))
-    
+
 def main(args):
     parser = OptionParser()
     parser.add_option('-a', '--add',
@@ -120,15 +120,15 @@ def main(args):
                       dest="lang",
                       help="Add a new language")
     parser.add_option('-l', '--list',
-                      action="store_true", 
+                      action="store_true",
                       dest="list",
                       help="List all supported languages")
     parser.add_option('-u', '--update',
-                      action="store_true", 
+                      action="store_true",
                       dest="update",
                       help="Update pot file and all po files")
     parser.add_option('-c', '--compile',
-                      action="store_true", 
+                      action="store_true",
                       dest="compile",
                       help="Compile all .po files into .mo")
     parser.add_option('-p', '--package',
@@ -154,7 +154,7 @@ def main(args):
         for lang in list_languages(root):
             print lang
         return
-    
+
     if options.update:
         update_po(root, package)
 
