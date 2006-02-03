a05cedf138bc2e6056186997d656c7a841f04e91
Johan Dahlin <jdahlin@async.com.br>
2006-02-03 17:15:40 +0000

    whitespace

--- kiwi/ui/icon.py
+++ kiwi/ui/icon.py
@@ -7,17 +7,17 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 # USA
-# 
+#
 # Author(s): Johan Dahlin <jdahlin@async.com.br>
 #
 
@@ -40,7 +40,7 @@
 # |+-----------------------------------+|
 # |-------------------------------------|  (4) cursor, black
 # |                                     |
-# +-------------------------------------| 
+# +-------------------------------------|
 #
 # So, now we want to put an icon in the edge:
 # An earlier approached by Lorzeno drew the icon directly on the text area,
@@ -48,15 +48,15 @@
 # entry the icon will be drawn on top of the text.
 # Now what we want to do is to resize the text area and create a
 # new window upon which we can draw the icon.
-# 
+#
 # +-------------------------------------+
 # |                                     |  (5) icon window
 # |+----------------------------++-----+|
-# || |-- /-\  |                 ||     ||  
+# || |-- /-\  |                 ||     ||
 # || |-  | |  |                 || (5) ||
-# || |   \-/  |                 ||     ||  
+# || |   \-/  |                 ||     ||
 # |+----------------------------++-----+|
-# |-------------------------------------|  
+# |-------------------------------------|
 # |                                     |
 # +-------------------------------------+
 #
@@ -81,7 +81,7 @@ class IconEntry(object):
     """
     Helper object for rendering an icon in a GtkEntry
     """
-    
+
     def __init__(self, entry):
         if not isinstance(entry, gtk.Entry):
             raise TypeError("entry must be a gtk.Entry")
@@ -104,16 +104,16 @@ class IconEntry(object):
             return
 
         self._entry.show_tooltip(entry)
-        
+
     def _on_entry__leave_notify_event(self, entry, event):
         if event.window != self.get_icon_window():
             return
-        
+
         self._entry.hide_tooltip()
-        
+
     def get_icon_window(self):
         return self._icon_win
-    
+
     def set_pixbuf(self, pixbuf):
         """
         @param pixbuf: a gdk.Pixbuf or None
@@ -124,7 +124,7 @@ class IconEntry(object):
             # to something visible, then set the pixbuf
             entry.connect_object('realize', self.set_pixbuf, pixbuf)
             return
-        
+
         if pixbuf:
             if not isinstance(pixbuf, gdk.Pixbuf):
                 raise TypeError("pixbuf must be a GdkPixbuf")
@@ -134,13 +134,13 @@ class IconEntry(object):
             if not self._pixbuf:
                 return
         self._pixbuf = pixbuf
-        
+
         if pixbuf:
             self._pixw = pixbuf.get_width()
             self._pixh = pixbuf.get_height()
         else:
             self._pixw = self._pixh = 0
-            
+
         win = self._icon_win
         if not win:
             self.construct()
@@ -158,7 +158,7 @@ class IconEntry(object):
         # Hack: This triggers a .recompute() which is private
         entry.set_visibility(entry.get_visibility())
         entry.queue_draw()
-    
+
     def construct(self):
         if self._constructed:
             return
@@ -169,16 +169,16 @@ class IconEntry(object):
 
         # Hack: Save a reference to the text area, now when its created
         self._text_area = entry.window.get_children()[0]
-        
+
         # PyGTK should allow default values for most of the values here.
-        win = gtk.gdk.Window(entry.window, 
+        win = gtk.gdk.Window(entry.window,
                              self._pixw, self._pixh,
                              gtk.gdk.WINDOW_CHILD,
                              (gtk.gdk.ENTER_NOTIFY_MASK |
                               gtk.gdk.LEAVE_NOTIFY_MASK),
                              gtk.gdk.INPUT_OUTPUT,
-                             'icon window', 
-                             0, 0, 
+                             'icon window',
+                             0, 0,
                              entry.get_visual(),
                              entry.get_colormap(),
                              gtk.gdk.Cursor(entry.get_display(), gdk.LEFT_PTR),
@@ -187,7 +187,7 @@ class IconEntry(object):
         win.set_user_data(entry)
         win.set_background(entry.style.base[entry.state])
         self._constructed = True
-        
+
     def deconstruct(self):
         if self._icon_win:
             # This is broken on PyGTK 2.6.x
@@ -197,37 +197,37 @@ class IconEntry(object):
                 pass
             # Destroy not needed, called by the GC.
             self._icon_win = None
-            
+
     def update_background(self, color):
         if not self._icon_win:
             return
 
         self._entry.modify_base(gtk.STATE_NORMAL, color)
-        
+
         self.draw_pixbuf()
-        
+
     def resize_windows(self):
         if not self._pixbuf:
             return
-        
+
         # Make space for the icon, both windows
         winw = self._entry.window.get_size()[0]
         textw, texth = self._text_area.get_size()
         textw = winw - self._pixw - 8
         self._text_area.resize(textw, texth)
-        
-        iconx = 4 + textw 
+
+        iconx = 4 + textw
         icony = 4
         icon_win = self._icon_win
         # XXX: Why?
         if not icon_win:
             return
-        
+
         # If the size of the window is large enough, resize and move it
         # Otherwise just move it to the right side of the entry
         if icon_win.get_size() != (self._pixw, self._pixh):
             icon_win.move_resize(iconx, icony, self._pixw, self._pixh)
-        else:    
+        else:
             icon_win.move(iconx, icony)
 
     def draw_pixbuf(self):
@@ -238,14 +238,14 @@ class IconEntry(object):
         # XXX: Why?
         if not win:
             return
-        
+
         # Draw background first
         color = self._entry.style.base_gc[self._entry.state]
         win.draw_rectangle(color, True,
                            0, 0, self._pixw, self._pixh)
-        
+
         # Always draw the icon, regardless of the window emitting the
         # event since makes it a bit smoother on resize
         win.draw_pixbuf(None, self._pixbuf, 0, 0, 0, 0,
                         self._pixw, self._pixh)
-        
+
