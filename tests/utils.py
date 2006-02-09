b710a5490ad5195db5c045af4c6d9fe5e4aa2976
Johan Dahlin <jdahlin@async.com.br>
2006-02-09 14:57:00 +0000

    Make it work with trial

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,18 @@
+import os
+import sys
+import time
+
 import gtk
 
-from time import sleep
-import sys
-
-sys.path.insert(0, "..")
 
 def refresh_gui(delay=0):
     while gtk.events_pending():
         gtk.main_iteration_do(block=False)
-    sleep(delay)
+    time.sleep(delay)
+
+dir = os.path.dirname(__file__)
+if not dir in sys.path:
+    sys.path.insert(0, os.path.join(dir))
+
+from kiwi.environ import environ
+environ.add_resource('glade', dir)
