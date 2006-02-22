717454d25378617080aa3707ab7306ea33aaf592
Johan Dahlin <jdahlin@async.com.br>
2006-02-22 19:05:34 +0000

    Add second diary example

--- examples/framework/diary/diary2.py
+++ examples/framework/diary/diary2.py
@@ -5,19 +5,28 @@ from kiwi.ui.objectlist import Column, ObjectList
 class DiaryEntry:
     title = ''
     text = ''
-    period = ''
+    period = 'morning'
+
+    def get_words(self):
+        return len(self.text.split())
+
+    def get_chars(self):
+        return len(self.text)
 
 class Diary(ProxyDelegate):
     def __init__(self):
-        self.entries = ObjectList([Column("title", width=120),
+        self.entries = ObjectList([Column("title", width=120, sorted=True),
                                    Column("period", width=80),
-                                   Column("text", expand=True)])
-
-        ProxyDelegate.__init__(self, DiaryEntry(), gladefile="diary",
+                                   Column("text", expand=True, visible=False)])
+        ProxyDelegate.__init__(self, DiaryEntry(), gladefile="diary2",
                                delete_handler=self.quit_if_last)
         self.hbox.pack_start(self.entries)
         self.entries.show()
         self.entries.grab_focus()
+        self.set_editable(False)
+
+    def proxy_updated(self, *args):
+        self.entries.update(self.model)
 
     def on_add__clicked(self, button):
         entry = DiaryEntry()
@@ -26,15 +35,30 @@ class Diary(ProxyDelegate):
         self.set_model(entry)
         self.entries.append(entry)
         self.title.grab_focus()
+        self.set_editable(True)
 
     def on_remove__clicked(self, button):
         entry = self.entries.get_selected()
         if entry:
             self.entries.remove(entry)
 
+        if len(self.entries) < 1:
+            editable = False
+        else:
+            editable = True
+        self.set_editable(editable)
+
+    def on_text__content_changed(self, *args):
+        self.update("chars")
+        self.update("words")
+        self.entries.update(self.model)
+
     def on_entries__selection_changed(self, entries, instance):
         self.set_model(instance)
 
+    def set_editable(self, editable):
+        self.leftbox.set_sensitive(editable)
+        self.remove.set_sensitive(editable)
+
 proxy = Diary()
 proxy.show_and_loop()
-
