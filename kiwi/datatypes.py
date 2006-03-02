e79c607309b6ad04432316bcc0c93b980933caf4
Johan Dahlin <jdahlin@async.com.br>
2006-03-02 21:11:19 +0000

    Break build

--- kiwi/datatypes.py
+++ kiwi/datatypes.py
@@ -181,6 +181,7 @@ class _IntConverter(BaseConverter):
         if format is None:
             format = '%d'
 
+        return lformat(format, value)
         # Do not use lformat here, since an integer should always
         # be formatted without thousand separators. Think of the
         # use case of a port number, "3128" is desired, and not "3,128"
