0941a4bbeef16b9e39d3e0f5357b06a0d4168531
Johan Dahlin <jdahlin@async.com.br>
2006-03-07 14:26:34 +0000

    do not grab

--- examples/validation/personalinformation.py
+++ examples/validation/personalinformation.py
@@ -82,7 +82,6 @@ proxy = form.add_proxy(person, ['name', 'age', 'birthdate',
                                 'height', 'weight', 'about',
                                 'nationality', 'status', 'gender'])
 form.show_all()
-form.birthdate.grab_focus()
 
 def on_ok_btn_clicked(widget):
     print "Name:", person.name
