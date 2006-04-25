8254ed014b69808b4e58fd5094560b4960563245
Johan Dahlin <jdahlin@async.com.br>
2006-04-25 15:33:20 +0000

    don't use deprecated features

--- examples/completion.py
+++ examples/completion.py
@@ -1,7 +1,7 @@
 # encoding: iso-8859-1
 import gtk
 
-from kiwi.ui.widgets.entry import Entry
+from kiwi.ui.widgets.entry import ProxyEntry
 
 def on_entry_activate(entry):
     print 'You selected:', entry.read()
@@ -14,23 +14,22 @@ vbox = gtk.VBox()
 win.add(vbox)
 
 # Normal entry
-entry = Entry()
+entry = ProxyEntry()
+entry.data_type = str
 entry.connect('activate', on_entry_activate)
-entry.set_completion_strings(['Belo Horizonte', u'São Carlos',
-                              u'São Paulo',  u'Båstad',
-                              u'Örnsköldsvik', 'sanca', 'sampa'])
+entry.prefill(['Belo Horizonte', u'São Carlos',
+               u'São Paulo',  u'Båstad',
+               u'Örnsköldsvik', 'sanca', 'sampa'])
 vbox.pack_start(entry)
 
-
-entry = Entry()
+entry = ProxyEntry()
+entry.data_type = int
 entry.connect('activate', on_entry_activate)
-entry.set_completion_strings(['Brazil', 'Sweden', 'China'],
-                             [186, 9, 1306])
-entry.set_completion_strings()
+entry.prefill([('Brazil', 186),
+               ('Sweden', 9),
+               ('China', 1306)])
 vbox.pack_start(entry)
 
 win.show_all()
-
-
 
 gtk.main()
