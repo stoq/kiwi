f5270453ebd6fa79ac946a64cabf82c5bee54c5f
Johan Dahlin <jdahlin@async.com.br>
2006-10-12 19:35:26 +0000

    Rework the logger, avoid using so much homebrew stuff. Do not create a stream handler for each logger. Add set_log_file() which can write all messages following a certain pattern to a file

--- kiwi/log.py
+++ kiwi/log.py
@@ -18,97 +18,84 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 # USA
 #
-# Author(s): Adriano Monteiro <adriano@globalret.com.br>
-#            Johan Dahlin     <jdahlin@async.com.br>
+# Author(s): Johan Dahlin     <jdahlin@async.com.br>
 #
 
 import fnmatch
 import logging
 import os
-import sys
+
+# Globals
+
+_console = None
+_filter = None
 
 class LogError(Exception):
     pass
 
-class Formatter(logging.Formatter):
-    def format(self, record):
-        # 1: format
-        # 2-6: logging module
-        # 7: log (log.info/log.warn etc)
-        # 8: callsite
-        frame = sys._getframe(8)
-        filename = os.path.basename(frame.f_code.co_filename)
-        record.msg = '%s:%d %s' % (filename, frame.f_lineno, record.msg)
-        return logging.Formatter.format(self, record)
+class Logger(object):
+    # Backwards compatibility, we should probably replace the callsites
+    # with import logging; logging.getLogger(name)
+    def __new__(self, name):
+        return logging.getLogger(name)
 
-class Logger(logging.Logger):
-    log_domain = 'default'
-    def __init__(self, name=None, level=logging.NOTSET):
-        """Initializes Log module, creating log handler and defining log
-        level. level attribute is not mandatory. It defines from which level
-        messages should be logged. Logs with lower level are ignored.
-
-        logging default levels table:
-
-        Level
-          - logging.NOTSET
-          - logging.DEBUG
-          - logging.INFO
-          - logging.WARNING
-          - logging.ERROR
-          - logging.CRITICAL
-        """
-        if not name:
-            name = Logger.log_name
-        logging.Logger.__init__(self, name, get_log_level(name))
-
-        stream_handler = logging.StreamHandler(sys.stdout)
-
-        # Formater class define a format for the log messages been
-        # logged with this handler
-        # The following format string
-        #   ("%(asctime)s (%(levelname)s) - %(message)s") will result
-        # in a log message like this:
-        #   2005-09-07 18:15:12,636 (WARNING) - (message!)
-        format_string = ("%(asctime)s %(message)s")
-        stream_handler.setFormatter(Formatter(format_string,
-                                              datefmt='%T'))
-        self.addHandler(stream_handler)
-
+class _Logger(logging.Logger):
     def __call__(self, message, *args, **kwargs):
         self.info(message, *args, **kwargs)
 
-_log_levels = {}
-_default_level = logging.WARNING
+logging.setLoggerClass(_Logger)
+
+class ReversedGlobalFilter(logging.Filter):
+    """
+    It's like a reversed filter, the default behavior
+    is to not show the message, you need to add custom filters for all
+    the records you wish to see
+    """
+    def __init__(self, filters=None):
+        """
+        @param filters: a list of tuples, (name, level),
+          name can include wildcards: * or ?
+        """
+        logging.Filter.__init__(self)
+        self.filters = filters or []
+
+    def add_filter(self, f, level=logging.DEBUG):
+        self.filters.append((f, level))
+
+    def filter(self, record):
+        for f, level in self.filters:
+            if fnmatch.fnmatch(record.name, f):
+                return True
+
+        return False
+
+def set_log_file(filename, mask=None):
+    """
+    @param filename:
+    @param mask: optional
+    """
+    stoq = logging.FileHandler(filename, 'w')
+    stoq.setFormatter(logging.Formatter(
+        '%(asctime)s %(name)-18s %(levelname)-8s %(message)s',
+        datefmt='%F %T'))
+    log = logging.getLogger()
+    log.addHandler(stoq)
+    log.setLevel(logging.DEBUG)
+
+    if mask:
+        stoq.addFilter(ReversedGlobalFilter([(mask, logging.DEBUG)]))
 
 def set_log_level(name, level):
     """
     @param name: logging category
     @param level: level
     """
-    global _log_levels
-    _log_levels[name] = level
+    global _filter
+    _filter.add_filter(name, level)
 
-def get_log_level(name):
-    """
-    @param name: logging category
-    @returns: the level
-    """
-    global _log_levels, _default_level
-
-    for category in _log_levels:
-        if fnmatch.fnmatch(name, category):
-            level = _log_levels[category]
-            break
-    else:
-        level = _default_level
-    return level
-
-def _read_log_level():
-    global _default_level
-
+def _read_log_levels(console_filter):
     log_levels = {}
-    # bootstrap issue, cannot depend on environ
+    # bootstrap issue, cannot depend on kiwi.environ
     log_level = os.environ.get('KIWI_LOG')
     if not log_level:
         return log_levels
@@ -130,13 +117,26 @@ def _read_log_level():
 
         level = 50 - (level * 10)
 
-        if name == '*':
-            _default_level = level
-            continue
-        log_levels[name] = level
+        console_filter.add_filter(name, level)
 
-    return log_levels
+def _create_console():
+    global _filter, _console
 
-_log_levels = _read_log_level()
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(logging.Formatter(
+        "%(asctime)s %(message)s", datefmt='%T'))
+    logging.getLogger().addHandler(console)
+
+    console_filter = ReversedGlobalFilter()
+    console.addFilter(console_filter)
+
+    _read_log_levels(console_filter)
+
+    # Set globals
+    _filter = console_filter
+    _console = console
+
+_create_console()
 
 kiwi_log = Logger('kiwi')
