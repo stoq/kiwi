e6dfd16cc5cb43a3dff4e34678173cd66e822249
Johan Dahlin <jdahlin@async.com.br>
2007-04-26 19:47:41 +0000

    * examples/entrymask.py: Add a new example

---
+++ examples/entrymask.py
@@ -0,0 +1,23 @@
+import sys
+
+import gtk
+
+from kiwi.ui.entry import KiwiEntry
+
+def main(args):
+    w = gtk.Window()
+    w.set_position(gtk.WIN_POS_CENTER)
+    w.set_size_request(250, -1)
+    w.set_title('ComboEntry example')
+    w.connect('delete-event', gtk.main_quit)
+
+    e = KiwiEntry()
+    e.set_mask('0000-00-00')
+    w.add(e)
+
+    w.show_all()
+    e.set_position(0)
+    gtk.main()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
