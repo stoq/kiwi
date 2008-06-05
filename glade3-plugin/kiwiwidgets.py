4a0a457998964331890354d8d262060d2d24b607
Johan Dahlin <jdahlin@async.com.br>
2008-06-05 21:36:11 +0000

    2008-06-05  Johan Dahlin  <jdahlin@async.com.br>
        * glade3-plugin/kiwiwidgets.py:
        * glade3-plugin/kiwiwidgets.xml:
        * glade3-plugin/setup.py:
        Update the glade3 plugin to Juan's latest revival
        of catalog support in glade3

--- glade3-plugin/kiwiwidgets.py
+++ glade3-plugin/kiwiwidgets.py
@@ -1,3 +1,7 @@
+import os
+import glob
+
+import gtk
 
 from kiwi.ui.hyperlink import HyperLink
 from kiwi.ui.objectlist import ObjectList, ObjectTree
@@ -25,6 +29,24 @@ ProxySpinButton
 ProxyTextView
 ProxyButton
 
+def _get_icon_path():
+    dirname = os.path.dirname(__file__)
+    if not os.path.exists(os.path.join(dirname, '.svn')):
+        prefix = os.path.join(dirname, '..', '..', '..')
+        icondir = os.path.join(prefix, 'share', 'glade3', 'pixmaps', '22x22')
+    else:
+        icondir = os.path.join(dirname, '..', 'gazpacho-plugin',
+                               'resources', 'kiwiwidgets')
+    return os.path.abspath(icondir)
+    
+def _register_icons():
+    icondir = _get_icon_path()
+    for filename in glob.glob(os.path.join(icondir, '*.png')):
+        basename = os.path.basename(filename)
+        name = basename[:-4]
+        gtk.icon_theme_add_builtin_icon(
+            'widget-kiwi-%s' % (name,),
+            22,
+            gtk.gdk.pixbuf_new_from_file(filename))
 
-
-
+_register_icons()
--- glade3-plugin/setup.py
+++ glade3-plugin/setup.py
@@ -14,21 +14,21 @@ def get_glade3_variable(variablename):
     return commands.getoutput('pkg-config --variable=%s %s' % (variablename,
                                                                GLADELIB))
 
-if glade3_exists():
-    catalogdir = get_glade3_variable('catalogdir')
-    moduledir = get_glade3_variable('moduledir')
-    pixmapdir = get_glade3_variable('pixmapdir')
-    pixmaps = listfiles('..', 'gazpacho-plugin',
-                        'resources', 'kiwiwidgets', '*.png')
-    setup(
-        data_files=[
-            (catalogdir, ['kiwiwidgets.xml']),
-            (moduledir, ['kiwiwidgets.py']),
-            (os.path.join(pixmapdir, '16x16'), pixmaps),
-            (os.path.join(pixmapdir, '22x22'), pixmaps),
-        ]
-    )
-else:
+if not glade3_exists():
     print 'Glade 3 is not installed, neither will be this plugin.'
+
+catalogdir = get_glade3_variable('catalogdir')
+moduledir = get_glade3_variable('moduledir')
+pixmapdir = get_glade3_variable('pixmapdir')
+pixmaps = listfiles('..', 'gazpacho-plugin',
+                        'resources', 'kiwiwidgets', '*.png')
+setup(
+    data_files=[
+        (catalogdir, ['kiwiwidgets.xml']),
+        (moduledir, ['kiwiwidgets.py']),
+        (os.path.join(pixmapdir, '16x16'), pixmaps),
+        (os.path.join(pixmapdir, '22x22'), pixmaps),
+    ]
+    )
 
 
