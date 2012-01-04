9781fc530ede146ab1351db6f9ca86d1a083491a
Johan Dahlin <jdahlin@async.com.br>
2012-01-04 11:21:45 -0200

    Rename package to kiwi-gtk, so it can be uploaded to pypi

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if sys.platform != 'win32' and not 'bdist_wininst' in sys.argv:
                                      libraries=libraries))
 
 pixmaps = listfiles('glade-plugin', 'resources', 'kiwiwidgets', '*.png')
-setup(name="kiwi",
+setup(name="kiwi-gtk",
       version=".".join(map(str, kiwi_version)),
       description="A framework and a set of enhanced widgets based on PyGTK",
       long_description=__doc__,
